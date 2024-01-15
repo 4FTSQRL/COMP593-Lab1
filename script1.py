5b474ec3f315a350ef0eee91d2e5dad27052f968
4FTSQRL <156530576+4FTSQRL@users.noreply.github.com>
2024-01-15 12:57:11 -0500

    Created script files
    Added code that prints lines from the Zen of Python.

---
+++ script1.py
@@ -0,0 +1,10 @@
+"""
+    Author: Elicia Ramitt (4FTSQRL)
+    Course: COMP593
+    Assignment: Lab 1
+    File: script1.py
+"""
+
+# Printing 2 Lines from the Zen of Python
+print(f"Flat is better than nested.")
+print(f"Namespaces are one honking great idea -- let's do more of those!")
---
+++ script2.py
@@ -0,0 +1,10 @@
+"""
+    Author: Elicia Ramitt (4FTSQRL)
+    Course: COMP593
+    Assignment: Lab 1
+    File: script2.py
+"""
+
+# Printing a few more lines from the Zen of Python
+print(f"Beautiful is better than ugly.")
+print(f"Now is better than never.")
