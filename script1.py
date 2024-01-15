547fcc107e0822881178aa0c076890952e77b122
4FTSQRL <156530576+4FTSQRL@users.noreply.github.com>
2024-01-15 13:03:16 -0500

    Edited comment in script1.py

--- script1.py
+++ script1.py
@@ -5,6 +5,6 @@
     File: script1.py
 """
 
-# Printing 2 Lines from the Zen of Python
+# Printing 2 lines from the Zen of Python
 print(f"Flat is better than nested.")
 print(f"Namespaces are one honking great idea -- let's do more of those!")
